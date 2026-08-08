2beff6697a5f3bfa87e42264fe5b966707ee7c8a
Celina25 <eaa25celu@students.eaaa.dk>
2026-08-08 19:16:09 +0200

    Tilføjelse af projekter og andet

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,13 @@
 const projects = [
   {
     slug: "portfolio",
-    title: "MARVEL- Civil War Poster",
+    title: "MARVEL - Poster",
     year: "2026",
     summary: "En personlig portfolio bygget med React, Vite og GitHub Pages.",
     description:
       "Portfolioen viser udvalgte projekter og fungerer som et udgangspunkt for at arbejde med komponenter, routing, styling og deployment.",
     tags: ["Adobe Photoshop", "Adobe Lightroom"],
-    image: `${import.meta.env.BASE_URL}billeder/poster.svg`,
+    image: `${import.meta.env.BASE_URL}billeder/marvel.png`,
     links: [
       {
         label: "Live site",
@@ -26,9 +26,26 @@ const projects = [
     summary:
       "Et projektkort, som du kan kopiere og ændre til dit eget projekt.",
     description:
-      "Beskriv problemet, processen, din rolle, de vigtigste valg og hvad du lærte. Gør projektet konkret, så andre kan forstå dit arbejde.",
+      "Ctrl er et fiktivt brand jeg kreerede som en del af et case study. Jeg arbejdede her med brugerreasearch for at forstå vores målgruppe, er derved finde frem til deres brandidentitet, samt hvordan man kunne markedsføre brandet på sociale medier, her Instagram. Jeg arbejdede i både Figma og forskellige Adobe programmer, for at komme frem til resulatet",
     tags: ["User Research", "Figma", "Brand Design/Visual Identity"],
     image: `${import.meta.env.BASE_URL}billeder/ctrl.logo.svg`,
+    links: [
+      {
+        label: "Eksempel-link",
+        href: "https://github.com",
+      },
+    ],
+  },
+  {
+    slug: "case-study",
+    title: "YATA- Poster",
+    year: "2025",
+    summary:
+      "Et projektkort, som du kan kopiere og ændre til dit eget projekt.",
+    description:
+      "Ctrl er et fiktivt brand jeg kreerede som en del af et case study. Jeg arbejdede her med brugerreasearch for at forstå vores målgruppe, er derved finde frem til deres brandidentitet, samt hvordan man kunne markedsføre brandet på sociale medier, her Instagram. Jeg arbejdede i både Figma og forskellige Adobe programmer, for at komme frem til resulatet",
+    tags: ["User Research", "Figma", "Brand Design/Visual Identity"],
+    image: `${import.meta.env.BASE_URL}billeder/YATA.svg`,
     links: [
       {
         label: "Eksempel-link",
