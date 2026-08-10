a43cc3ebf10ea550564f4fe452c8a0357553a96e
Celina25 <eaa25celu@students.eaaa.dk>
2026-08-10 16:55:39 +0200

    Contact Page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,38 +2,39 @@ import "./ContactPage.css";
 
 function ContactPage() {
   return (
-    <div className="page narrow contact-page">
-      <h1>Hvor du finder mig</h1>
+    <div className="contact-page">
+      <h1 className="contact-heading">KONTAKT</h1>
 
-      <div className="contact-card">
-        <div className="contact-card-image">
-          <img src="public/billeder/mig-blurry.svg" alt="billede af mig" />
+      <div className="contact-section">
+        <div className="contact-card">
+          <div className="contact-card-content"></div>
         </div>
+      </div>
 
-        <div className="contact-card-content">
-          <h2>Kontakt</h2>
-          <p>
-            Du er velkommen til at skrive til mig, hvis du vil samarbejde om et
-            projekt eller høre mere om mig og mit arbejde.
-          </p>
-
-          <ul className="contact-list">
-            <li>
-              <a href="celinaml@outlook.dk">celinaml@outlook.dk</a>
-            </li>
-            <li>
-              <a
-                href="https://github.com/celinamlunding"
-                rel="noreferrer"
-                target="_blank"
-              >
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="tel:+4512345678">+45 60 24 85 56</a>
-            </li>
-          </ul>
+      <div className="additional-section">
+        <div className="additional-content">
+          <div className="additional-image">
+            <img src="public/billeder/mig-blurry.svg" alt="billede af mig" />
+          </div>
+          <div className="additional-text">
+            <ul className="contact-list">
+              <li>
+                <a href="mailto:celinaml@outlook.dk">celinaml@outlook.dk</a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/celinamlunding"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a href="tel:+4512345678">+45 60 24 85 56</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
