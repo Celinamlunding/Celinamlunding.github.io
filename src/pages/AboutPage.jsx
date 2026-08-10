f94de2de0d13f0044db214e2f8d98d06ecf8bd94
Celina25 <eaa25celu@students.eaaa.dk>
2026-08-10 21:56:03 +0200

    Om mig siden

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -5,21 +5,37 @@ function AboutPage() {
     <div className="about-page">
       <h1 className="about-heading">OM MIG</h1>
 
-      <div className="about-section"></div>
-
+      <div className="about-section">
+        <img src="svg/bog.svg" alt="" />
+        <img src="svg/guitar.svg" alt="" />
+        <img src="svg/headphones.svg" alt="" />
+        <img src="svg/kaffe.svg" alt="" />
+        <img src="svg/kamera.svg" alt="" />
+        <img src="svg/phone.svg" alt="" />
+      </div>
       <div className="about-additional-section">
         <div className="about-additional-content">
           <div className="about-additional-text">
             <p>
-              Jeg elsker at fange de specialle øjeblikke på kamerea, store som
-              små.
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
+
             <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat.
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
             </p>
+
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </p>
+          </div>
+
+          <div className="about-images">
+            <img src="collage/korea.JPG" alt="Book" />
+            <img src="collage/byen.jpg" alt="Music" />
+            <img src="collage/MIG.png" alt="Portrait" />
           </div>
         </div>
       </div>
